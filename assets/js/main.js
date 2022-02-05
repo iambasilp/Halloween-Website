35c5a43fd1ea98a4433e742508f39bd9fd6a9e9f
iambasilp <basilxg321@gmail.com>
2022-02-05 19:18:57 +0530

    scroll up section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,3 +79,11 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
